1de05a9e80428150c00d6976ed147f19fdbe2469
LennonCMJ <LennonCMJ@live.com>
2017-03-23 23:41:40 +0800

    HelloWorld Commit

---
+++ HelloWorld/HelloWorld.asm
@@ -0,0 +1,25 @@
+; HelloWorld.asm
+; Author : Lennon Chia
+
+global 
+
+section .text
+
+_start:
+	; print hello world 
+	mov eax, 0x4
+	mov ebx, 0x1
+	mov ecx, message
+	mov edx, mlen
+	int 0x80
+
+	; exit the program gracefully
+	mov eax, 0x1
+	mov ebx, 0x5
+	int 0x80	
+
+section .data
+	
+	message: db "Hello World!"
+	mlen	equ  $-message
+
